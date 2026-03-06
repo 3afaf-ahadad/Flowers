becdcbeb082444832ae46a5e9dd52f626871f13f
Afaf <afaf20ahadad@gmail.com>
2026-03-06 08:00:00 +0000

    Revert "Add AuthController and CheckUserSession middleware with login/logout functionality"
    This reverts commit c516d4c4baad1464184b0cab65be0604e8dd7f78.

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-// pages de connexion/déconnexion
-Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login'])->name('login');
-Route::post('/logout', [\App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
-
-// routes protégées par session utilisateur
-Route::middleware('check.usersession')->group(function () {
-    Route::get('/documents', [\App\Http\Controllers\DocumentController::class, 'index']);
-    Route::get('/upload', [\App\Http\Controllers\DocumentController::class, 'create']);
-    // ajouter d'autres routes /documents, /upload selon besoin
-});
